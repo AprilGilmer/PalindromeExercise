ed00e6d30181c1206d4126bcb2c4d831b5f4afd2
April Gilmer <aprilgilmer1@gmail.com>
2025-04-23 12:42:24 -0500

    Completed Palindrome Exercise 2

--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,33 @@
+using PalindromeExercise;
 using System;
 using Xunit;
 
-namespace PalindromeExerciseTests
-{
-    public class UnitTest1
-    {
-        [Fact]
-        public void Test1()
-        {
+namespace PalindromeExerciseTests;
 
-        }
+public class WordSmith_Tests
+{
+    [Theory]
+    [InlineData("racecar", true)]
+    [InlineData("Racecar", true)]
+    [InlineData("hannah", true)]
+    [InlineData("kayak", true)]
+    [InlineData("Lula", false)]
+    [InlineData("April", false)]
+    [InlineData("Maya", false)]
+    [InlineData("", false)]
+    [InlineData(null, false)]
+
+    public void IsAPalidrome_Test(string word, bool expected)
+    {
+        //Arrange
+        var ws = new WordSmith();
+
+        //Act
+        var actual = ws.IsAPalindrome(word);
+        //Assert
+        Assert.Equal(expected, actual);
     }
+
+
+
 }
